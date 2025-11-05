7d3d93e4392d90275a705915e0879a86854e23f5
Martin Krebs <krebsmato@gmail.com>
2025-11-05 19:34:07 +0100

    fix: correct count overrides on merge

--- cs336_basics/bpe.py
+++ cs336_basics/bpe.py
@@ -91,21 +91,9 @@ def pretokenize(
 
     for partial_byte_pair_to_pretoken_index in partial_byte_pair_to_pretoken_indices:
         for byte_pair, pretoken_index in partial_byte_pair_to_pretoken_index.items():
-            byte_pair_to_pretoken_index[byte_pair] = byte_pair_to_pretoken_index[byte_pair] | pretoken_index
+            byte_pair_to_pretoken_index[byte_pair].update(pretoken_index)
 
     return pretoken_counts, byte_pair_counts, byte_pair_to_pretoken_index
-
-
-def max_pair(pretoken_counts: dict[Pretoken, int]) -> BytePair:
-    pair_counts: dict[BytePair, int] = defaultdict(int)
-
-    for pretoken, count in pretoken_counts.items():
-        for i in range(len(pretoken) - 1):
-            pair_counts[(pretoken[i], pretoken[i + 1])] += count
-
-    _, pair = max([(v, k) for k, v in pair_counts.items()])
-
-    return pair
 
 
 def merge_pretoken(byte_pair: BytePair, merge: bytes, pretoken: Pretoken) -> Pretoken:
@@ -131,54 +119,34 @@ def overwrite_pretoken_counts(
 ) -> None:
     affected_pretokens = list(byte_pair_to_pretoken_index[max_pair])
 
-    # print("merge: ", max_pair)
-
-    # print("affected: ", affected_pretokens)
-
     for affected_pretoken in affected_pretokens:
         count = pretoken_counts[affected_pretoken]
         merged_pretoken = merge_pretoken(max_pair, merge, affected_pretoken)
 
-        affected_byte_pairs: dict[BytePair, int] = defaultdict(int)
+        pairs: dict[BytePair, int] = defaultdict(int)
 
         for i in range(len(affected_pretoken) - 1):
-            j = i + 1
+            pair = affected_pretoken[i], affected_pretoken[i + 1]
+            pairs[pair] += 1
 
-            current_byte_pair = (affected_pretoken[i], affected_pretoken[j])
-            assert affected_pretoken in byte_pair_to_pretoken_index[current_byte_pair], (
-                current_byte_pair,
+        for pair, pair_count in pairs.items():
+            byte_pair_counts[pair] -= pair_count * count
+
+            assert affected_pretoken in byte_pair_to_pretoken_index[pair], (
+                pair,
                 affected_pretoken,
+                byte_pair_counts[pair],
+                byte_pair_to_pretoken_index[pair],
             )
-
-            if current_byte_pair == max_pair:
-                if i > 0:
-                    left_pair = affected_pretoken[i - 1], affected_pretoken[i]
-                    if left_pair != max_pair:
-                        affected_byte_pairs[left_pair] += 1
-                if j < len(merged_pretoken) - 1:
-                    right_pair = affected_pretoken[j], affected_pretoken[j + 1]
-                    if right_pair != max_pair:
-                        affected_byte_pairs[right_pair] += 1
-
-        for affected_byte_pair, byte_pair_in_affected_pretoken_count in affected_byte_pairs.items():
-            byte_pair_counts[affected_byte_pair] -= count * byte_pair_in_affected_pretoken_count
-
-            assert affected_pretoken in byte_pair_to_pretoken_index[affected_byte_pair], (
-                affected_byte_pair,
-                affected_pretoken,
-                byte_pair_counts[affected_byte_pair],
-                byte_pair_to_pretoken_index[affected_byte_pair],
-            )
-            byte_pair_to_pretoken_index[affected_byte_pair].remove(affected_pretoken)
+            byte_pair_to_pretoken_index[pair].remove(affected_pretoken)
 
         pretoken_counts[merged_pretoken] += count
         del pretoken_counts[affected_pretoken]
 
         for i in range(len(merged_pretoken) - 1):
             pair = (merged_pretoken[i], merged_pretoken[i + 1])
-            if pair[0] == merge or pair[1] == merge:
-                byte_pair_counts[pair] += count
-                byte_pair_to_pretoken_index[pair].add(merged_pretoken)
+            byte_pair_counts[pair] += count
+            byte_pair_to_pretoken_index[pair].add(merged_pretoken)
 
     del byte_pair_counts[max_pair]
     del byte_pair_to_pretoken_index[max_pair]
@@ -197,13 +165,13 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]) -> tu
 
     rounds = vocab_size - len(vocab)
 
-    # TODO: make sure to stop when we there is not enough pairs to merge
-    for i in tqdm(range(rounds)):
-        __, pair = max([(v, k) for k, v in byte_pair_counts.items()])
-        
-        # print(f"it: {i}, pair: {pair}")
-        # if i == 2:
-        #     break
+    for _ in tqdm(range(rounds)):
+        pair_counts = [(v, k) for k, v in byte_pair_counts.items()]
+
+        if not pair_counts:
+            break
+
+        _count, pair = max(pair_counts)
 
         merge = pair[0] + pair[1]
         vocab[next_token_id] = merge
