3484f50bb088708ad7a09b66322763a0fabf9112
Martin Krebs <krebsmato@gmail.com>
2025-11-06 17:25:29 +0100

    feat: scripts to tokenize data

---
+++ tokenize_tiny_stories.py
@@ -0,0 +1,10 @@
+from cs336_basics.bpe import train_bpe
+
+vocab_size = 10000
+special_token = "<|endoftext|>"
+file_path = "data/TinyStoriesV2-GPT4-train.txt"
+
+if __name__ == "__main__":
+    _ = train_bpe(file_path, vocab_size, [special_token])
+
+
