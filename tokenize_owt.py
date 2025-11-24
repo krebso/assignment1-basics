5062360fce520cee3126f3c7466a2ad8bfc273a0
Martin Krebs <krebsmato@gmail.com>
2025-11-24 19:56:28 +0100

    feat: save tokenized data

--- tokenize_owt.py
+++ tokenize_owt.py
@@ -1,10 +1,13 @@
 from cs336_basics.bpe import train_bpe
+import pickle
 
 vocab_size = 32000
 special_token = "<|endoftext|>"
 file_path = "data/owt_train.txt"
 
 if __name__ == "__main__":
-    _ = train_bpe(file_path, vocab_size, [special_token])
+    vocab, merges = train_bpe(file_path, vocab_size, [special_token])
+    pickle.dump(vocab, open("owt_vocab.json", "wb"))
+    pickle.dump(merges, open("owt_merges.json", "wb"))
 
 
--- tokenize_tiny_stories.py
+++ tokenize_tiny_stories.py
@@ -1,3 +1,4 @@
+import pickle
 from cs336_basics.bpe import train_bpe
 
 vocab_size = 10000
@@ -5,6 +6,8 @@ special_token = "<|endoftext|>"
 file_path = "data/TinyStoriesV2-GPT4-train.txt"
 
 if __name__ == "__main__":
-    _ = train_bpe(file_path, vocab_size, [special_token])
+    vocab, merges = train_bpe(file_path, vocab_size, [special_token])
+    pickle.dump(vocab, open("tiny_stories_vocab.json", "wb"))
+    pickle.dump(merges, open("tiny_stories_merges.json", "wb"))
 
 
