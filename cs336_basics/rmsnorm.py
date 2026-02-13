fbb1ff7ad1e32862894c4934f95958024be9f6d9
Martin Krebs <krebsmato@gmail.com>
2026-02-13 20:34:14 +0100

    feat: rmsnorm

---
+++ cs336_basics/rmsnorm.py
@@ -0,0 +1,27 @@
+from typing import final, override
+import torch
+from torch.nn import Module, Parameter
+from einops import einsum, reduce
+
+
+@final
+class RMSNorm(Module):
+    def __init__(
+        self, d_model: int, eps: float = 1e-5, device: torch.device | None = None, dtype: torch.dtype | None = None
+    ) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.eps = eps
+        g = torch.ones(self.d_model)
+        self.g = Parameter(g)
+
+    @override
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        in_dtype = x.dtype
+        upcast_x = x.to(torch.float32)
+
+        sum = reduce(torch.square(upcast_x), "... in_features -> ... 1", "sum")
+        rms = torch.sqrt((sum + self.eps) / self.d_model)
+        mul = einsum(upcast_x, self.g, "... in_features, in_features -> ... in_features")
+
+        return (mul / rms).to(in_dtype)
--- tests/adapters.py
+++ tests/adapters.py
@@ -13,6 +13,7 @@ from cs336_basics.bpe import train_bpe
 from cs336_basics.linear import Linear
 from cs336_basics.embedding import Embedding
 from cs336_basics.tokenizer import Tokenizer
+from cs336_basics.rmsnorm import RMSNorm
 
 
 def run_linear(
@@ -390,7 +391,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    rmsnorm = RMSNorm(d_model, eps)
+    _ = rmsnorm.load_state_dict({"g": weights})
+    return rmsnorm.forward(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
