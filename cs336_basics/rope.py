0261fde9ae0aaef46a077d72839b17f555705981
Martin Krebs <krebsmato@gmail.com>
2026-02-16 19:37:03 +0100

    feat: register buffers for rope

--- cs336_basics/rope.py
+++ cs336_basics/rope.py
@@ -8,21 +8,22 @@ from einops import einsum, rearrange
 
 class RotaryPositionalEmbedding(Module):
     def __init__(self, theta: float, d_k: int, max_seq_len: int, device=torch.device | None) -> None:
+        super().__init__()
         i = torch.arange(max_seq_len)
         k = torch.arange(d_k // 2)
         exponents = (2 * k) / d_k
         freq = 1 / (theta**exponents)
         angles = torch.outer(i, freq)
 
-        self.sin = torch.sin(angles)
-        self.cos = torch.cos(angles)
+        self.register_buffer("sin", torch.sin(angles), persistent=False)
+        self.register_buffer("cos", torch.cos(angles), persistent=False)
 
     def forward(self, x: Tensor, token_positions: Tensor) -> Tensor:
         x_even = x[..., 0::2]
         x_odd = x[..., 1::2]
 
-        sin = self.sin[token_positions]
-        cos = self.cos[token_positions]
+        sin = self.sin[token_positions]  # type: ignore
+        cos = self.cos[token_positions]  # type: ignore
 
         y_even = x_even * cos - x_odd * sin
         y_odd = x_even * sin + x_odd * cos
