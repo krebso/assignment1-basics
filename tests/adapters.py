3684e117a727bac984e5e4ce0a874468692c8eea
Martin Krebs <krebsmato@gmail.com>
2026-03-15 18:56:37 +0100

    feat: adapters

--- tests/adapters.py
+++ tests/adapters.py
@@ -23,6 +23,8 @@ from cs336_basics.transformer_block import TransformerBlock
 from cs336_basics.llm import Transformer
 from cs336_basics.cross_entropy import cross_entropy
 from cs336_basics.optim import AdamW
+from cs336_basics.lr_scheduling import cosine_lr_schedule
+from cs336_basics.gradient_clipping import gradient_clipping
 
 
 def run_linear(
@@ -557,7 +559,7 @@ def run_gradient_clipping(parameters: Iterable[torch.nn.Parameter], max_l2_norm:
 
     The gradients of the parameters (parameter.grad) should be modified in-place.
     """
-    raise NotImplementedError
+    return gradient_clipping(parameters, max_l2_norm)
 
 
 def get_adamw_cls() -> Any:
@@ -592,7 +594,7 @@ def run_get_lr_cosine_schedule(
     Returns:
         Learning rate at the given iteration under the specified schedule.
     """
-    raise NotImplementedError
+    return cosine_lr_schedule(it, max_learning_rate, min_learning_rate, warmup_iters, cosine_cycle_iters)
 
 
 def run_save_checkpoint(
